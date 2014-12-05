870902264d5f78120b271dc5d891cbf3bc73e476
James Roper <james@jazzy.id.au>
2014-12-05 14:43:58 +1100

    Really fixed docs resolvers

--- project/Common.scala
+++ project/Common.scala
@@ -27,6 +27,7 @@ object Common extends AutoPlugin {
     fork in Test := true,
 
     resolvers ++= DefaultOptions.resolvers(snapshot = true),
-    resolvers += Resolver.typesafeRepo("releases")
+    resolvers += Resolver.typesafeRepo("releases"),
+    resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases" // specs2 depends on scalaz-stream
   )
 }
