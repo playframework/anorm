2d5a9f858df53695f48ba6f9cb98cf6e12406fb9
James Roper <james@jazzy.id.au>
2015-05-14 14:47:21 +1000

    Switched to interplay build plugin

--- project/Common.scala
+++ project/Common.scala
@@ -1,32 +1,17 @@
 import sbt.Keys._
 import sbt._
 import sbt.plugins.JvmPlugin
-import com.typesafe.sbt.SbtScalariform._
-import sbtrelease.ReleasePlugin._
-import com.typesafe.sbt.pgp.PgpKeys
-import interplay.Omnidoc.Import._
 
 object Common extends AutoPlugin {
   override def trigger = allRequirements
   override def requires = JvmPlugin
 
   override def projectSettings = Seq(
-    organization := "com.typesafe.play",
-
-    scalaVersion := sys.props.get("scala.version").getOrElse("2.10.4"),
-    crossScalaVersions := Seq("2.10.4", "2.11.6"),
-
-    scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8"),
-    (javacOptions in compile) := Seq("-source", "1.8", "-target", "1.8"),
-    (javacOptions in doc) := Seq("-source", "1.8"),
-
-    fork in Test := true,
-
-    resolvers ++= DefaultOptions.resolvers(snapshot = true),
-    resolvers += Resolver.typesafeRepo("releases"),
     resolvers += "Scalaz Bintray Repo" at {
       "http://dl.bintray.com/scalaz/releases" // specs2 depends on scalaz-stream
-    }
+    },
+    scalacOptions ~= (_.filterNot(_ == "-Xfatal-warnings")),
+    fork in Test := true
   )
 }
 
@@ -80,55 +65,4 @@ private[anorm] trait FunctionAdapter {
       }
     }
   }
-}
-
-object Publish extends AutoPlugin {
-  override def trigger = noTrigger
-  override def requires = interplay.Omnidoc
-
-  override def projectSettings = Release.settings ++ Seq(
-    publishTo := {
-      if (isSnapshot.value) Some(Opts.resolver.sonatypeSnapshots)
-      else Some(Opts.resolver.sonatypeStaging)
-    },
-    homepage := Some(url("https://github.com/playframework/anorm")),
-    licenses := Seq("Apache 2" -> url(
-      "http://www.apache.org/licenses/LICENSE-2.0")),
-    pomExtra := {
-  <scm>
-    <url>https://github.com/playframework/anorm</url>
-    <connection>scm:git:git@github.com:playframework/anorm.git</connection>
-  </scm>
-  <developers>
-    <developer>
-      <id>playframework</id>
-      <name>Play Framework Team</name>
-      <url>https://github.com/playframework</url>
-    </developer>
-  </developers>
-    },
-    pomIncludeRepository := { _ => false },
-
-    OmnidocKeys.githubRepo := "playframework/anorm",
-    OmnidocKeys.tagPrefix := ""
-  ) ++ scalariformSettings
-}
-
-object NoPublish extends AutoPlugin {
-  override def trigger = noTrigger
-  override def requires = JvmPlugin
-
-  override def projectSettings = Release.settings ++ Seq(
-    publish := (),
-    publishLocal := (),
-    publishTo := Some(Resolver.file("no-publish", crossTarget.value / "no-publish"))
-  )
-}
-
-object Release {
-  def settings = releaseSettings ++  Seq(
-    ReleaseKeys.crossBuild := true,
-    ReleaseKeys.publishArtifactsAction := PgpKeys.publishSigned.value,
-    ReleaseKeys.tagName := (version in ThisBuild).value
-  )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,13 +1,11 @@
-resolvers += Resolver.typesafeRepo("releases")
 resolvers ++= DefaultOptions.resolvers(snapshot = true)
 
-addSbtPlugin("com.typesafe.play" % "interplay" % "0.1.0")
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.5")
-addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
-addSbtPlugin("com.typesafe.play" % "play-docs-sbt-plugin" % sys.props.getOrElse("play.version", "2.4.0-M2"))
+addSbtPlugin("com.typesafe.play" % "interplay" % "1.0.1")
+addSbtPlugin("com.typesafe.play" % "play-docs-sbt-plugin" % sys.props.getOrElse("play.version", "2.4.0-RC2"))
 addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "0.1.6")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.3.0")
 // This release doesn't consider "requires" to be a keyword
+resolvers += Resolver.typesafeRepo("releases")
 libraryDependencies += "org.scalariform" %% "scalariform" % "0.1.5-20140822-69e2e30"
 
