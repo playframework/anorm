50a446128f6a6e1ac17f0b39e45411151e6910dd
cchantep <chantepie@altern.org>
2016-11-26 21:38:54 +0100

    Show pretty string representation about query

---
+++ tokenizer/src/main/scala/anorm/Show.scala
@@ -0,0 +1,27 @@
+package anorm
+
+/** Typeclass for string representation */
+trait Show {
+  def show: String
+}
+
+object Show {
+  trait Maker[T] extends (T => Show) {
+    /** Show maker for the appropriate subject type. */
+    def apply(subject: T): Show
+  }
+
+  object Maker {
+    object Identity extends Maker[Show] {
+      def apply(s: Show): Show = s
+    }
+  }
+
+  /** Returns the string representation for the given subject. */
+  def mkString[T](subject: T)(implicit maker: Maker[T]): String =
+    maker(subject).show
+}
+
+final class StringShow(underlying: String) extends Show {
+  def show = underlying
+}
---
+++ tokenizer/src/main/scala/anorm/StatementToken.scala
@@ -0,0 +1,67 @@
+package anorm
+
+private[anorm] sealed trait StatementToken
+
+private[anorm] case object PercentToken extends StatementToken {
+  override val toString = "%"
+}
+
+private[anorm] case class StringToken(value: String) extends StatementToken {
+  override lazy val toString = s""""${value.replaceAll("\"", "\\\"")}""""
+}
+
+private[anorm] object StatementToken {
+  implicit object StatementTokenShowMaker extends Show.Maker[StatementToken] {
+    def apply(token: StatementToken): Show = token match {
+      case StringToken(value) => new StringShow(value)
+      case _ => new StringShow(token.toString)
+    }
+  }
+}
+
+/**
+ * @param prepared Already prepared tokens, not requiring to rewrite placeholder.
+ * @param placeholder Optional placeholder (name), after already prepared tokens
+ */
+private[anorm] class TokenGroup(
+    val prepared: List[StatementToken],
+    val placeholder: Option[String]) extends Product with Serializable {
+
+  def copy(prepared: List[StatementToken] = this.prepared, placeholder: Option[String] = this.placeholder): TokenGroup = new TokenGroup(prepared, placeholder)
+
+  val productArity = 2
+
+  def productElement(n: Int): Any = n match {
+    case 1 => prepared
+    case 2 => placeholder
+  }
+
+  def canEqual(that: Any): Boolean = that.isInstanceOf[TokenGroup]
+
+  override def equals(that: Any): Boolean = that match {
+    case other: TokenGroup =>
+      (prepared, placeholder).equals(other.prepared -> other.placeholder)
+
+    case _ => false
+  }
+
+  override def hashCode: Int = (prepared, placeholder).hashCode
+
+  override lazy val toString = s"TokenGroup($prepared, $placeholder)"
+}
+
+object TokenGroup extends scala.runtime.AbstractFunction2[List[StatementToken], Option[String], TokenGroup] {
+
+  def apply(prepared: List[StatementToken], placeholder: Option[String]): TokenGroup = new TokenGroup(prepared, placeholder)
+
+  def unapply(group: TokenGroup): Option[(List[StatementToken], Option[String])] = Some(group.prepared -> group.placeholder)
+
+  final class TokenGroupShow(group: TokenGroup) extends Show {
+    def show: String = group.prepared.map(Show.mkString(_)).
+      mkString + group.placeholder.fold("")(s => s"{$s}")
+  }
+
+  implicit object ShowMaker extends Show.Maker[TokenGroup] {
+    def apply(subject: TokenGroup): Show = new TokenGroupShow(subject)
+  }
+}
