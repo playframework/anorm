d463d0fe07e6861b5de9ae967d3ec89d0750b987
Scala Steward <me@scala-steward.org>
2021-11-02 11:58:15 +0100

    Update silencer-lib, silencer-plugin to 1.7.7

--- project/Common.scala
+++ project/Common.scala
@@ -31,7 +31,7 @@ object Common extends AutoPlugin {
       scalaVersion.value, 12,
       (Test / sourceDirectory).value),
     libraryDependencies ++= {
-      val silencerVer = "1.7.6"
+      val silencerVer = "1.7.7"
 
       Seq(
         compilerPlugin(("com.github.ghik" %% "silencer-plugin" % silencerVer).
