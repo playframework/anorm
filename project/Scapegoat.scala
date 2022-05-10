4735f2c69b9edad4b9660a2022b0c88e4b4750df
Scala Steward <me@scala-steward.org>
2022-05-10 12:43:50 +0200

    Update scalac-scapegoat-plugin to 1.4.14

--- project/Scapegoat.scala
+++ project/Scapegoat.scala
@@ -7,7 +7,7 @@ object Scapegoat {
   import ScapegoatSbtPlugin.autoImport._
 
   val settings = Seq(
-    ThisBuild / scapegoatVersion             := "1.4.13",
+    ThisBuild / scapegoatVersion             := "1.4.14",
     ThisBuild / scapegoatReports             := Seq("text"),
     ThisBuild / scapegoatDisabledInspections := Seq("FinalModifierOnCaseClass"),
     pomPostProcess := transformPomDependencies { dep =>
