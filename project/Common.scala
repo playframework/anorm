901ea9d7e26b3ba4d34d227b4ae9a950c7b3a7a1
Scala Steward <scala_steward@virtuslab.com>
2023-06-10 20:37:56 +0000

    Update scala-library, scala-reflect to 2.12.18

--- project/Common.scala
+++ project/Common.scala
@@ -22,7 +22,7 @@ object Common extends AutoPlugin {
   override def projectSettings = Seq(
     organization        := "org.playframework.anorm",
     sonatypeProfileName := "org.playframework",
-    scalaVersion        := "2.12.17",
+    scalaVersion        := "2.12.18",
     crossScalaVersions  := Seq("2.11.12", scalaVersion.value, "2.13.10", "3.3.0"),
     (Compile / unmanagedSourceDirectories) ++= {
       val sv = scalaVersion.value
