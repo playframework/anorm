a289fb2cd3a352ca680d319ed5e58307ecdcc445
Scala Steward <scala_steward@virtuslab.com>
2023-07-10 21:22:43 +0000

    Scala 3.3.1 / 2.13.12

--- project/Common.scala
+++ project/Common.scala
@@ -23,7 +23,7 @@ object Common extends AutoPlugin {
     organization        := "org.playframework.anorm",
     sonatypeProfileName := "org.playframework",
     scalaVersion        := "2.12.18",
-    crossScalaVersions  := Seq("2.11.12", scalaVersion.value, "2.13.11", "3.3.1"),
+    crossScalaVersions  := Seq("2.11.12", scalaVersion.value, "2.13.12", "3.3.1"),
     (Compile / unmanagedSourceDirectories) ++= {
       val sv = scalaVersion.value
 
@@ -32,7 +32,7 @@ object Common extends AutoPlugin {
     (Test / unmanagedSourceDirectories) ++= scalaUnmanaged(scalaVersion.value, (Test / sourceDirectory).value),
     ThisBuild / libraryDependencies ++= {
       if (scalaBinaryVersion.value != "3") {
-        val silencerVersion = "1.7.13"
+        val silencerVersion = "1.7.14"
 
         Seq(
           compilerPlugin(
